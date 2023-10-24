a4cab988aca358ad15d821a838572a6b52d080ee
iamphanh <iamphanh2912@gmail.com>
2023-10-24 16:07:23 +0700

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'true/base_account',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6eec78397083689b7b20f038cec50cdc7121209b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'true/base_account' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6eec78397083689b7b20f038cec50cdc7121209b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
